3824cd28a2f2f54a8e5317f5b15705fce8fb8ec4
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-01 18:36:48 +0100

    Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var ints = File.ReadAllLines("input.txt")
+                    .Select(s => Int32.Parse(s)).ToList();
+
+                var howManyIntsToSum = 3;
+                var requiredSum = 2020;
+
+                var solution = Solve(ints, howManyIntsToSum, requiredSum);
+                Console.WriteLine(solution);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        // General-purpose recursive solution for finding the product of a set of integers
+        // in a list that add up to a given sum.
+        static int Solve(IEnumerable<int> ints, int howManyIntsToSum, int requiredSum)
+        {
+            if (howManyIntsToSum == 0 || !ints.Any()) return 0;
+            var head = ints.First();
+            var tail = ints.Skip(1);
+            if (howManyIntsToSum == 1 && head == requiredSum) return (head);
+            var result = head * Solve(tail, howManyIntsToSum - 1, requiredSum - head);
+            if (result != 0) return result;
+            return Solve(tail, howManyIntsToSum, requiredSum);
+        }
+    }
+}
