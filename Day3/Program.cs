451aba5f7eac4e8a9eea99d64a8fed8f08696fc0
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-03 14:36:58 +0100

    Day 3 Part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var xPos = 0;
+            var treeCount = 0;
+
+            foreach (var row in input)
+            {
+                xPos = xPos % row.Length;
+                if (row[xPos] == '#')
+                {
+                    treeCount++;
+                }
+                xPos += 3;
+            }
+
+            Console.WriteLine(treeCount);
+        }
+    }
+}
