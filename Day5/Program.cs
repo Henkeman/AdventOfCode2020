435320d9f5aa5903bc881e1f76a9dc9fdf7fdb79
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-05 12:51:08 +0100

    Day 5 Part 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -9,7 +9,30 @@ namespace Day5
         static void Main(string[] args)
         {
             var boardingPasses = File.ReadAllLines("input.txt").Select(s => new BoardingPass(s)).ToArray();
-            Console.WriteLine(boardingPasses.OrderByDescending(x => x.SeatId).First().SeatId);
+                       
+            var mySeat = FindEmptySeat(boardingPasses);
+
+            Console.WriteLine(mySeat);
+        }
+
+        private static int FindEmptySeat(BoardingPass[] boardingPasses)
+        {
+            var seatIds = boardingPasses.Select(s => s.SeatId).ToHashSet();
+
+            var firstSeatId = boardingPasses.Select(s => s.SeatId).OrderBy(s => s).First();
+            var lastSeatId = boardingPasses.Select(s => s.SeatId).OrderByDescending(s => s).First();
+
+            firstSeatId++; // we're not in the first seat
+            lastSeatId--; // we're not in the last seat
+
+            for (int i = firstSeatId; i < lastSeatId; i++)
+            {
+                if (seatIds.Contains(i-1) && !seatIds.Contains(i) && seatIds.Contains(i+1))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 
