0cb2ac444b2eee0a4718871c176a709bd3086513
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-06 13:16:14 +0100

    Day 6 Part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            int count = CountAnswers(input);
+            Console.WriteLine(count);
+        }
+
+        private static int CountAnswers(string[] input)
+        {
+            int count = 0;
+            var set = new HashSet<char>();
+
+            foreach (var line in input)
+            {
+                if (line.Trim().Length > 0)
+                {
+                    foreach (var ch in line)
+                    {
+                        set.Add(ch);
+                    }
+                }
+                else
+                {
+                    count += set.Count;
+                    set.Clear();
+                }
+            }
+            count += set.Count;
+
+            return count;
+        }
+    }
+}
