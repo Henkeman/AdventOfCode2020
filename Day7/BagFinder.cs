fa3ea6318f55755144071603cb2fca47f5946d57
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-09 17:38:07 +0100

    Day 7 Part 1

---
+++ Day7/BagFinder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    class BagFinder
+    {
+        public Dictionary<string, List<Bag>> AllBags { get; internal set; }
+
+        public BagFinder(string[] input)
+        {
+            AllBags = GetBagDictionary(input);
+        }
+
+        private Dictionary<string, List<Bag>> GetBagDictionary(string[] input)
+        {
+            var dict = new Dictionary<string, List<Bag>>();
+            foreach (var row in input)
+            {
+                var bagrow = row.Replace(" bags", "").Replace(" bag", "").Replace(".", ""); //row cleanup
+                var bagContent = new List<Bag>();
+
+                var bagSplit = bagrow.Split(" contain ");
+                var bagName = bagSplit[0];
+
+                if (bagSplit[1] != "no other")
+                {
+                    var contents = bagSplit[1].Split(", ");
+                    foreach (var item in contents)
+                    {
+                        var pos = item.IndexOf(' ');
+                        var count = Int32.Parse(item.Substring(0, pos));
+                        var color = item.Substring(pos).Trim();
+                        bagContent.Add(new Bag(color, count));
+                    }
+                }
+                dict.Add(bagName, bagContent);
+            }
+            return dict;
+        }
+        private bool FindBag(string bag, string color)
+        {
+            var bagQueue = new Queue<Bag>();
+            GetChildBags(bag).ToList().ForEach(bagQueue.Enqueue);
+
+            while (bagQueue.Count > 0)
+            {
+                var queuedBag = bagQueue.Dequeue();
+                if (queuedBag.Color == color)
+                {
+                    return true;
+                }
+                else
+                {
+                    GetChildBags(queuedBag.Color).ToList().ForEach(bagQueue.Enqueue);
+                }
+            }
+
+            return false;
+        }
+
+        private List<Bag> GetChildBags(string color)
+        {
+            AllBags.TryGetValue(color, out var childBags);
+            return childBags;
+        }
+
+        internal int CountInAllBags(string color)
+        {
+            var count = 0;
+
+            foreach (var bag in AllBags.Keys)
+            {
+                if (FindBag(bag, color))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public class Bag
+        {
+            public string Color { get; set; }
+            public int Count { get; set; }
+            public Bag(string color, int count)
+            {
+                Color = color;
+                Count = count;
+            }
+        }
+    }
+}
--- Day7/Program.cs
+++ Day7/Program.cs
@@ -6,45 +6,17 @@ using System.Linq;
 namespace Day7
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
             // dark purple bags contain 4 dim salmon bags, 4 faded maroon bags, 2 drab red bags, 1 clear bronze bag.
             // dim green bags contain no other bags.
             var input = File.ReadAllLines("input.txt");
-
-            var allBags = GetBagDictionary(input);
-
-            var c = 0;
+            var bagFinder = new BagFinder(input);
+            
+            var c = bagFinder.CountInAllBags("shiny gold");
 
             Console.WriteLine(c);
-        }
-
-        private static Dictionary<string, Dictionary<string, int>> GetBagDictionary(string[] input)
-        {
-            var dict = new Dictionary<string, Dictionary<string, int>>();
-            foreach (var row in input)
-            {
-                var bagrow = row.Replace(" bags", "").Replace(" bag", "").Replace(".", ""); //row cleanup
-                var bagContent = new Dictionary<string, int>();
-
-                var bagSplit = bagrow.Split(" contain ");
-                var bagName = bagSplit[0];
-
-                if (bagSplit[1] != "no other")
-                {
-                    var contents = bagSplit[1].Split(", ");
-                    foreach (var item in contents)
-                    {
-                        var pos = item.IndexOf(' ');
-                        var count = Int32.Parse(item.Substring(0, pos));
-                        var color = item.Substring(pos).Trim();
-                        bagContent.Add(color, count);
-                    }
-                }
-                dict.Add(bagName, bagContent);
-            }
-            return dict;
         }
     }
 }
