cd30d49fb8476e8443977bfdadbbeaa403949227
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-09 23:05:02 +0100

    Day 9 Init

---
+++ Day9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+        }
+    }
+}
