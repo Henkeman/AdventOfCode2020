5a2aa08a84f4ff853c0852e1e2a347330c7df5c4
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-10 23:55:03 +0100

    Day 10 Init

---
+++ Day10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines("input.txt");
+        }
+    }
+}
