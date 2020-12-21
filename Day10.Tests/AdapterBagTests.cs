9bd58654911f0298ac77cc826d6f465e0b8707ad
Henrik Forsberg <henrikforsberg@hotmail.com>
2020-12-22 00:19:02 +0100

    Joined input lines

--- Day10.Tests/AdapterBagTests.cs
+++ Day10.Tests/AdapterBagTests.cs
@@ -11,18 +11,7 @@ namespace Day10.Tests
         public void Part1_Example1_Should_Be_35()
         {
             // Arrange
-            var inputs = new string[] {
-            "16",
-            "10",
-            "15",
-            "5",
-            "1",
-            "11",
-            "7",
-            "19",
-            "6",
-            "12",
-            "4" };
+            var inputs = new string[] { "16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4" };
 
             // Act
             adapterBag = new AdapterBag(inputs);
@@ -36,38 +25,8 @@ namespace Day10.Tests
         public void Part1_Example2_Should_Be_220()
         {
             // Arrange
-            var inputs = new string[] {
-            "28",
-            "33",
-            "18",
-            "42",
-            "31",
-            "14",
-            "46",
-            "20",
-            "48",
-            "47",
-            "24",
-            "23",
-            "49",
-            "45",
-            "19",
-            "38",
-            "39",
-            "11",
-            "1",
-            "32",
-            "25",
-            "35",
-            "8",
-            "17",
-            "7",
-            "9",
-            "4",
-            "2",
-            "34",
-            "10",
-            "3" };
+            var inputs = new string[] { "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19", "38", "39", "11",
+                "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3" };
 
             // Act
             adapterBag = new AdapterBag(inputs);
@@ -81,18 +40,7 @@ namespace Day10.Tests
         public void Part2_Example1_Arrangements_Should_Be_8()
         {
             // Arrange
-            var inputs = new string[] {
-            "16",
-            "10",
-            "15",
-            "5",
-            "1",
-            "11",
-            "7",
-            "19",
-            "6",
-            "12",
-            "4" };
+            var inputs = new string[] { "16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4" };
 
             // Act
             adapterBag = new AdapterBag(inputs);
@@ -105,38 +53,8 @@ namespace Day10.Tests
         public void Part2_Example2_Arrangements_Should_Be_19208()
         {
             // Arrange
-            var inputs = new string[] {
-            "28",
-            "33",
-            "18",
-            "42",
-            "31",
-            "14",
-            "46",
-            "20",
-            "48",
-            "47",
-            "24",
-            "23",
-            "49",
-            "45",
-            "19",
-            "38",
-            "39",
-            "11",
-            "1",
-            "32",
-            "25",
-            "35",
-            "8",
-            "17",
-            "7",
-            "9",
-            "4",
-            "2",
-            "34",
-            "10",
-            "3" };
+            var inputs = new string[] { "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45",
+                "19", "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3" };
 
             // Act
             adapterBag = new AdapterBag(inputs);
